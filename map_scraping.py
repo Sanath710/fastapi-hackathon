64db3e8c993e2561871bce2d16bbf4d718c4c215
Sanath Ezhuthachan <90824508+Sanath710@users.noreply.github.com>
2022-10-05 03:00:35 +0530

    Update map_scraping.py

--- map_scraping.py
+++ map_scraping.py
@@ -1,17 +1,15 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-chrome_options.add_argument("headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
+opts = webdriver.ChromeOptions()
+opts.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 
-browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+opts.add_argument("--headless")
+opts.add_argument("--no-sandbox")
+opts.add_argument("--disable-dev-shm-usage")
+
+browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=opts)
 
 def find_nearest(lat, lon, search) :
     
