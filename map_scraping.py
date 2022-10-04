306c2a7d34dfe78b1d5953538dfb2e75d6cf736c
Sanath710 <90824508+Sanath710@users.noreply.github.com>
2022-10-05 01:31:52 +0530

    Update map_scraping.py

--- map_scraping.py
+++ map_scraping.py
@@ -1,13 +1,14 @@
 from selenium import webdriver
 import os
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+options = Options()
+options.add_argument("start-maximized")
+browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+browser.get("https://www.google.com")
 
 def find_nearest(lat, lon, search) :
     
