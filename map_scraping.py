b82c9a880246981b29a83ef4925e61d1236955ff
Sanath Ezhuthachan <90824508+Sanath710@users.noreply.github.com>
2022-10-05 02:36:33 +0530

    Update map_scraping.py

--- map_scraping.py
+++ map_scraping.py
@@ -1,9 +1,6 @@
-from selenium import webdriver
 import os
-from selenium.webdriver.common.action_chains import ActionChains
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
